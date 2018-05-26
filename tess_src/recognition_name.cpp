b0bbbdd11ed455ec93bb18968f3957afe3b9e75d
tlimkim <limkim4233@gmail.com>
2018-05-26 16:30:29 +0900

    recognize several frame img

--- tess_src/recognition_name.cpp
+++ tess_src/recognition_name.cpp
@@ -5,6 +5,10 @@
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
 
+#define FILE_ 139
+
+using namespace std;
+
 int main()
 {
     char *outText;
@@ -15,13 +19,19 @@ int main()
       fprintf(stderr, "Could not initialize tesseract.\n");
 	    exit(1);
     }
-
+/*
     // Open input image with leptonica library
-    Pix *image = pixRead("./sample_pixel_image.jpg");
+    string input;
+    input = "/home/tlimkim/workspace/kbo/image/vfc_sample_2/0.jpg";
+    
+    const char* c = input.c_str();
+
+    //input = "jpg";
+    //Pix *image = pixRead("/home/tlimkim/workspace/kbo/image/vfc_sample_2/image_00000000%d.jpg", 0);
+    Pix *image = pixRead(c);
   
     api->SetImage(image);
     // Get OCR result
-    //api->SetRectangle(455, 685, 125, 55);
     outText = api->GetUTF8Text();
 
     printf("Batter Name: %s \n", outText);
@@ -30,6 +40,29 @@ int main()
     api->End();
     delete [] outText;
     pixDestroy(&image);
+    */
+    for (int i = 0; i < FILE_; i++) {
+      api->Init("/usr/share/tesseract-ocr/tessdata", "kor");
+
+      string input_img = "/home/tlimkim/workspace/kbo/image/vfc_sample_2/";
+      string num = std::to_string(i);
+      
+      input_img = input_img + num + ".jpg";
+
+      const char * str = input_img.c_str();
+
+      //printf("%s \n", str);
+      Pix *image = pixRead(str);
+      api->SetImage(image);
+
+      outText = api->GetUTF8Text();
+
+      printf("Batter Name: %s \n", outText);
+
+      api->End();
+      delete [] outText;
+      pixDestroy(&image);
+    }
 
     return 0;
 }
